2f1b68c4202d5abf98a3092a4e06dea7b8b7d39c
mexiar <hola@alejandrosena.com>
2023-03-17 13:35:04 -0600

    Operaciones básicas en consola

---
+++ js.js
@@ -0,0 +1,15 @@
+function suma (numA, numB) {
+    return numA + numB;
+}
+
+function resta (numA, numB) {
+    return numA - numB;
+}
+
+function division (numA, numB) {
+    return numA / numB;
+}
+
+function multiplicacion (numA, numB) {
+    return numA * numB;
+}
